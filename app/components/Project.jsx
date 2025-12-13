e81dcc0ec8905f414d765da35513c889c5ca650c
Marvel123g <marvellousolorunwa23@gmail.com>
2025-12-13 14:14:06 +0100

    Fixed a bug in portfolio website card under project section

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -53,10 +53,10 @@ export default function Project() {
             </div>
 
             <div className="view d-flex gap-2">
-              <a href="https://github.com/Marvel123g/my-portfolio.git ">
-                View Project
+              <a href="https://marvelousdev.vercel.app">View Project</a>
+              <a href="https://github.com/Marvel123g/my-portfolio.git">
+                View Code
               </a>
-              <a href="https://marvelousdev.vercel.app/">View Code</a>
             </div>
           </div>
         </div>
